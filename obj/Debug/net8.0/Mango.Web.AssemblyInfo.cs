4422e0e1bf68166c8ba394426085f3177076cbc7
ggonzalez1985 <gabrielgonzalez1985@gmail.com>
2024-10-31 18:10:06 -0300

    Cambios menores en Mango_Web.

--- obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f83115e0ed499e3d093be72eb1e10d8e7a9237f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5db7b94deda58f5334c7d2b8f1c002cde1c8853")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
