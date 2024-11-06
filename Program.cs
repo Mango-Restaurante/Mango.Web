6987c2cd5f7b2e7ca4042d050a4fd5d9bf8b7ee9
ggonzalez1985 <gabrielgonzalez1985@gmail.com>
2024-11-06 17:09:37 -0300

    IAuthService + AuthService - Finalizados

--- Program.cs
+++ Program.cs
@@ -8,13 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
-builder.Services.AddHttpClient<ICouponService, CouponService>(); //estas con las 3 lineas de arriba para usar iny de dependencia
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IAuthService, AuthService>();//estas con las 3 lineas de arriba para usar iny de dependencia + AddScoped
 
 //le asigna la url de Coupon API (localhost:7001)
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+//le asigna la url de Auth API (localhost:7002)
+SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5db7b94deda58f5334c7d2b8f1c002cde1c8853")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64464bedc31536d15948ffed452b0d3a4b950003")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
