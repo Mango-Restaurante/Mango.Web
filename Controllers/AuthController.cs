0abeff37bde9100f1087f7f9173cb5ebe05ac60c
ggonzalez1985 <gabrielgonzalez1985@gmail.com>
2024-11-08 10:27:28 -0300

    1)- Action Methods "Register" + "Login". 2)- Interfaz "ITokenProvider" + implementacion "TokenProvider".

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -22,6 +23,26 @@ namespace Mango.Web.Controllers
 			return View(loginRequestDto);
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> Login (LoginRequestDto obj)
+		{
+			ResponseDto responseDto = await _authService.LoginAsync(obj);
+
+			if (responseDto != null && responseDto.isSuccess)
+			{
+				LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ModelState.AddModelError("CustomerError", responseDto.Message);
+				return View(obj);
+			}
+
+		}
+
+
 		[HttpGet]
 		public IActionResult Register()
 		{
@@ -33,6 +54,39 @@ namespace Mango.Web.Controllers
 
 			ViewBag.RoleList = roleList;
 			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegistrationRequestDto obj)
+		{
+			ResponseDto result = await _authService.RegisterAsync(obj);
+			ResponseDto assingRole;
+
+			if (result != null && result.isSuccess)
+			{
+				if (string.IsNullOrEmpty(obj.Role))
+				{
+					obj.Role = SD.RoleCustomer;
+				}
+
+				assingRole = await _authService.AssignRoleAsync(obj);
+
+				if (assingRole != null && assingRole.isSuccess)
+				{
+					TempData["success"] = "Registration Successful";
+					return RedirectToAction(nameof(Login));
+				}
+			}
+
+			var roleList = new List<SelectListItem>()
+			{
+				new SelectListItem{Text= SD.RoleAdmin, Value= SD.RoleAdmin},
+				new SelectListItem{Text= SD.RoleCustomer, Value= SD.RoleCustomer},
+			};
+
+			ViewBag.RoleList = roleList;
+			return View(obj);
 		}
 
 
--- obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6987c2cd5f7b2e7ca4042d050a4fd5d9bf8b7ee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4171aec422052f838b7231267d840596b5562794")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
