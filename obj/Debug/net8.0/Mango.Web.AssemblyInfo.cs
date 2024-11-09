4378a9f292838a68f3bd4497c8a15e3ad4c159c2
ggonzalez1985 <gabrielgonzalez1985@gmail.com>
2024-11-09 14:44:42 -0300

    SignInUser - metodo finalizado.

--- obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4171aec422052f838b7231267d840596b5562794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abeff37bde9100f1087f7f9173cb5ebe05ac60c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
